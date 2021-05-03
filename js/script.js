268eb44f02c255b5c071f22a980a56ec087d5043
Bruno Henrique Silva Contente <42575725+brunohenriquecontente@users.noreply.github.com>
2021-05-03 13:33:27 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+var resultado = document.getElementById('resultado')
+resultado.innerHTML = '0'
+
+var btn1 = document.getElementById('btn1')
+
+
+function limpaTela() {
+    document.getElementById('resultado').innerHTML = '0'
+}
