dbc3b50f6df32b0015598201ac51b732d2c13d40
Bruno Henrique Silva Contente <42575725+brunohenriquecontente@users.noreply.github.com>
2021-05-03 15:04:35 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 
 var resultado = document.getElementById('resultado')
-resultado.innerHTML = '0'
+resultado.innerHTML = '0' 
 
-var btn1 = document.getElementById('btn1')
+var btn = document.getElementsByTagName('td')
 
 
+btn.addEventListener('click', function (){
+    resultado.innerHTML = btn.value
+
+})
+
+
+
+
+// função limpar a tela
 function limpaTela() {
     document.getElementById('resultado').innerHTML = '0'
 }
